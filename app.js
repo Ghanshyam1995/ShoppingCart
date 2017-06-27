52263fa9dedcae22509e2e1b85a040b2afa3b649
Nihkil <nikhil.l@flatworldsolutions.com>
2017-06-27 14:30:28 +0530

    Added extras view

--- app.js
+++ app.js
@@ -4,25 +4,25 @@ var sql = require("mssql");
 var cookieParser = require('cookie-parser');
 var session = require("express-session");
 var bodyParser = require('body-parser');
+var expressValidator = require('express-validator');
+var compression = require('compression');
 var app = express();
 var product = require("./routes/product");
 var account = require("./routes/account");
 var config = require("./configs/sql");
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(expressValidator());
 var port = process.env.port || 3000;
-
+app.use(compression());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cookieParser());
 app.use(session({ secret: "shopping-cart" }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-
 app.use('/products', product);
-
 app.use('/account', account);
 app.use('/', (req, res) => {
     sql.connect(config, (err) => {
@@ -33,10 +33,27 @@ app.use('/', (req, res) => {
             res.render('index', { title: "Home", records: record.recordset })
         })
     })
-
 });
 
-app.listen(port, () => {
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
 
+
+// error handler
+app.use(function(err, req, res, next) {
+
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render('404', { title: 'Page not Found' });
+});
+app.listen(port, () => {
     console.log("App listening on port :" + port);
 })
--- routes/account.js
+++ routes/account.js
@@ -2,6 +2,7 @@ var express = require("express");
 const router = express.Router();
 var sql = require("mssql");
 var config = require("../configs/sql");
+
 router.get("/login", (req, res, next) => {
     res.render('login', { title: "Login" })
 });
@@ -10,16 +11,44 @@ router.get("/login", (req, res, next) => {
 router.post('/login', (req, res, next) => {
     var email = req.body.email;
     var password = req.body.password;
-    sql.connect(config, (err) => {
-        var request = new sql.Request()
-            .input("Email", sql.NVarChar, email)
-            .input("Password", sql.NVarChar, password);
-        request.query("SELECT * from Users WHere Email=@Email AND Password=@Password", (err, user) => {
-            if (err) throw err;
-            req.session.user = user.recordset;
 
-        });
+    req.checkBody('email', 'Email is required').notEmpty();
+    req.checkBody('email', 'Email must be valid').isEmail();
+    req.checkBody('password', 'Password is required').notEmpty();
 
-    })
+    var errors = req.validationErrors();
+    if (errors) {
+        res.render('login', { errors: errors });
+    } else {
+        sql.connect(config, (err) => {
+            var request = new sql.Request()
+                .input("Email", sql.NVarChar, email)
+                .input("Password", sql.NVarChar, password);
+            request.query("SELECT * from Users WHere Email=@Email AND Password=@Password", (err, user) => {
+                if (err) throw err;
+                req.session.user = user.recordset;
+
+            });
+
+        })
+    }
 });
+
+router.all('/cart', (req, res) => {
+    res.render("cart", { title: "Cart", CartItems: req.session.cart });
+})
+
+router.get('/add-to-cart/:Id', (req, res) => {
+    var productId = req.params.Id;
+    sql.connect(config, (err) => {
+        if (err) throw err;
+        var request = new sql.Request().input("@ID", sql.Int, productId)
+            .query("SELECT * from Products WHere ID=ID", (err, product) => {
+                sql.close();
+                if (product)
+                    req.session.cart = product.recordset;
+            });
+    })
+})
+
 module.exports = router;
